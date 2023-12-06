4575f7379cb44706e43f16ae214cbed884a2fb97
Hariharan G <107925755+HariG007@users.noreply.github.com>
2023-12-06 21:08:09 +0530

    Add files via upload

---
+++ src/Routees.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/home/Home';
+import LogStatus from '../src/pages/LogStatus/LogStatus';
+import DataSubscriptionPage from './pages/DataSubcription/DataSubcription';
+import UserSettings from './pages/UserSettings/UserSettings';
+
+export default function Routees() {
+  return (
+    <div>
+ <BrowserRouter>
+  <Routes>
+    {/* Default route to Login */}
+    <Route exact path="/" element={<Home/>} />
+    {/* Users routes */}
+    <Route exact path="/logstatus" element={<LogStatus/>} />
+    <Route exact path="/datasubcription" element={<DataSubscriptionPage/>} />
+    <Route exact path="/usersettings" element={<UserSettings/>} />
+  </Routes>
+</BrowserRouter>
+    </div>
+  )
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import "./home.scss";
+import DashboardDefault from '../../components/Dashboardcomp/index';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <button style={{padding:'3px',marginLeft:'20px', margin:'10px',backgroundColor:' #f9688d',color:'white'}}>Statistic</button>
+        <DashboardDefault/>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Home;
